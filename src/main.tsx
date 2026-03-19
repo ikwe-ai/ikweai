b4e9f57f7c28821105e9d3d237b7f825134888f3
Stephanie <stephanie@taskbytask.net>
2026-03-18 20:08:18 -0500

    hg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/tokens.css";
+import "./styles/shared-components.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
