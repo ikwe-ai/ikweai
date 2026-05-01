d396896ccaaa809be17abaec8d7c9fed87cdc1be
Stephanie Stranko <stephanie@ikwe.ai>
2026-05-01 15:29:57 -0500

    Security cleanup: remove sensitive docs, dead Netlify config, node_modules

--- archive/react-app/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/tokens.css";
-import "./styles/shared-components.css";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
